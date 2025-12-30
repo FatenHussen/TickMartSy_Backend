d50fbd78d59faf45bc3a539c4a6408fd141a2447
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2025-12-30 15:35:27 +0300

    product management database

---
+++ app/Models/CategoryDetail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Translatable\HasTranslations;
+
+class CategoryDetail extends Model
+{
+    use HasFactory, HasTranslations;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+    ];
+
+    public array $translatable = [
+        'name',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    */
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(ProductCategoryDetail::class);
+    }
+}
---
+++ app/Models/ProductCategoryDetail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Translatable\HasTranslations;
+
+class ProductCategoryDetail extends Model
+{
+    use HasFactory, HasTranslations;
+
+    protected $fillable = [
+        'category_detail_id',
+        'product_id',
+        'detail_value',
+    ];
+
+    public array $translatable = [
+        'detail_value',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    */
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function categoryDetail()
+    {
+        return $this->belongsTo(CategoryDetail::class);
+    }
+}
