5840ac86d9bf5ebd6370e7b8b0dbd6b1865d60b6
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2026-01-18 13:17:41 +0300

    basketItem resources

--- app/Http/Resources/Basket/BasketItemCompanyResource.php
+++ app/Http/Resources/Basket/BasketItemCompanyResource.php
@@ -15,10 +15,7 @@ class BasketItemCompanyResource extends JsonResource
             'has_custom_price' => $this->hasCustomPrice(),
             'effective_price' => 
             $this->effective_price !== null
-                ?
-                 round($this->effective_price, 2)
-                : round($this->basketItem->price, 2)
-                ,
+                ? round($this->effective_price, 2): round($this->basketItem->price, 2),
         ];
     }
 }
