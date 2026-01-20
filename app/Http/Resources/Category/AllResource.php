0ddd5ae54dac348e850f188f28f2c331a1776f3d
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2026-01-20 12:32:06 +0300

    commit

--- app/Http/Resources/Category/AllResource.php
+++ app/Http/Resources/Category/AllResource.php
@@ -17,12 +17,6 @@ class AllResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'icon' => $this->icon,
-            'parent' => $this->whenLoaded('parent', function (){
-                return [
-                    'id' => $this->parent?->id,
-                    'name' => $this->parent?->name,
-                ];
-            }),
             'children' => $this->whenLoaded('children', function () {
                 return $this->children->map(function ($child)  {
                     return [
