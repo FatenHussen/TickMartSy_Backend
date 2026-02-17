ac7de481336e56cf2593417db550a2194ccbe93d
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2026-02-17 16:24:18 +0300

    basket user

--- app/Services/User/CategoryService.php
+++ app/Services/User/CategoryService.php
@@ -17,4 +17,23 @@ class CategoryService extends BaseService
         $this->relations = ['parent', 'children'];
         $this->pagination = true;
     }
+
+    protected function applyFilters($query, $filters)
+    {
+        // Apply name filter
+        if (isset($filters['name'])) {
+            $query->where('name', 'like', '%' . $filters['name'] . '%');
+        }
+
+        // Apply parent_id filter
+        if (array_key_exists('parent_id', $filters)) {
+            if ($filters['parent_id'] === null) {
+                $query->whereNull('parent_id');
+            } else {
+                $query->where('parent_id', $filters['parent_id']);
+            }
+        }
+
+        return $query;
+    }
 }
