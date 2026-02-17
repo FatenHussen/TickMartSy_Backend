6370ee826191b9a4acc5299cae760a9dc4808807
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2026-02-17 16:28:54 +0300

    basket user

--- app/Http/Resources/Product/OneResource.php
+++ app/Http/Resources/Product/OneResource.php
@@ -26,7 +26,7 @@ class OneResource extends JsonResource
             'model' => $this->model,
             'barcode' => $this->barcode,
             'time_prepare' => optional($this->time_prepare)->format('H:i'),
-            'bought_with'=>  AllResource::collection($this->boughtWithProduct()),
+            'bought_with'=>  AllResource::collection($this->bought_with_products_list),
             'is_instant_delivery' => $this->is_instant_delivery,
             'rating' => $this->average_rating ?? 0,
             'rating_breakdown' => $this->getRatingBreakdown() ?? [],
@@ -62,7 +62,7 @@ class OneResource extends JsonResource
                     'name' => $shop->name,
                 ];
             }),
-            
+
         ];
     }
 
