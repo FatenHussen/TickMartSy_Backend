5c3c263021248448b808a231521780c2d626e4b0
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2026-02-24 12:36:36 +0300

    exchange points +vendor +edit search

--- app/Http/Requests/User/Category/FilterRequest.php
+++ app/Http/Requests/User/Category/FilterRequest.php
@@ -36,7 +36,7 @@ class FilterRequest extends FormRequest
         return [
             'name' => 'nullable|string',
             'parent_id' => 'nullable|integer|exists:categories,id',
-            'search'=> ['nullable', 'string', 'max:255'],
+            'search' => 'nullable|string',
             'shop_id' => 'nullable|integer|exists:shops,id',
             'type' => 'nullable|in:new,most_popular,top_rated',
         ];
