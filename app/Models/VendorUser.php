915298883a83b05dbd60b23c814dae97e64d2960
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2026-03-12 16:11:51 +0300

    edit notification dashboard

--- app/Models/VendorUser.php
+++ app/Models/VendorUser.php
@@ -38,16 +38,12 @@ class VendorUser extends Authenticatable
     public function notifications()
     {
         return $this->morphMany(VendorNotification::class, 'notifiable')
-            ->where('notifiable_id', $this->id)
-            ->where('notifiable_type', self::class)
             ->orderBy('created_at', 'desc');
     }
 
     public function unreadNotifications()
     {
         return $this->morphMany(VendorNotification::class, 'notifiable')
-            ->where('notifiable_id', $this->id)
-            ->where('notifiable_type', self::class)
             ->whereNull('read_at')
             ->orderBy('created_at', 'desc');
     }
@@ -55,8 +51,6 @@ class VendorUser extends Authenticatable
     public function readNotifications()
     {
         return $this->morphMany(VendorNotification::class, 'notifiable')
-            ->where('notifiable_id', $this->id)
-            ->where('notifiable_type', self::class)
             ->whereNotNull('read_at')
             ->orderBy('created_at', 'desc');
     }
