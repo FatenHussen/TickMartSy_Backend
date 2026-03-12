1f8a6fde5048e9f4db1b58af3680b5fead47dd33
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2026-03-12 14:06:19 +0300

    vendor dashboard notification

--- resources/views/filament/firebase-script.blade.php
+++ resources/views/filament/firebase-script.blade.php
@@ -1,3 +1,53 @@
-<script>
-console.log("🔥 FCM SCRIPT LOADED");
+<script type="module">
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging.js";
+console.log('test');
+const firebaseConfig = {
+    apiKey: "AIzaSyCaWSRgKaqd0P__owf8MtZLhdInskytXKo",
+    authDomain: "tikmool-app-3241.firebaseapp.com",
+    projectId: "tikmool-app-3241",
+    storageBucket: "tikmool-app-3241.firebasestorage.app",
+    messagingSenderId: "786190897596",
+    appId: "1:786190897596:web:5a3eaba811e0f45141dbb9"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+async function initFCM() {
+
+    const permission = await Notification.requestPermission();
+
+    if (permission !== "granted") {
+        return;
+    }
+
+    const token = await getToken(messaging, {
+        vapidKey: "BCMDGVeweEPiu0hfisL2YH2Nzwlmr6P6sEsxfKibl20AmWdgVg2DfmzjWGPm8g-tI2sLMbPls1bMjsItYvF3R88"
+    });
+
+    console.log("FCM TOKEN:", token);
+
+    await fetch("/vendor/fcm-token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').content
+        },
+        body: JSON.stringify({
+            token: token
+        })
+    });
+
+}
+
+initFCM();
+
+onMessage(messaging, (payload) => {
+    new Notification(payload.notification.title, {
+        body: payload.notification.body
+    });
+});
+
 </script>
