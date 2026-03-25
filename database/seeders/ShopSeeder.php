a5d13d3e3305b0239c9f68502ef3ad3f06838ad7
Aya Alghadban <116346559+ayalghadban@users.noreply.github.com>
2026-03-25 09:09:21 +0300

    edits

--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -72,6 +72,7 @@ class ShopSeeder extends Seeder
                 'ratings_sum'   => rand(0, 500),
 
                 'vendor_id' => $vendor->id,
+                'is_free_delivery' => 1
             ]);
 
 
