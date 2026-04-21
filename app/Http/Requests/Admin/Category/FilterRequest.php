295a359b8beb6f93fe5e6fe8c13c37f194ca7fd8
Aya Alghadban <ayalghadban@gmail.com>
2026-04-22 00:51:29 +0300

    edits

--- app/Http/Requests/Admin/Category/FilterRequest.php
+++ app/Http/Requests/Admin/Category/FilterRequest.php
@@ -15,6 +15,22 @@ class FilterRequest extends FormRequest
     }
 
     /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        if (!$this->has('parent_id')) {
+            return;
+        }
+
+        $parentId = $this->input('parent_id');
+
+        if ($parentId === '' || $parentId === null || strtolower((string) $parentId) === 'null') {
+            $this->merge(['parent_id' => null]);
+        }
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
